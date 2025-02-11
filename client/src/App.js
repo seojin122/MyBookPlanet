183523a1d91bef1ae9f7000a50ca75d2fc684d32
차차 <0409democracy@gmail.com>
2025-02-11 22:32:26 +0900

    fix, design:Fix missing link and modify main page UI
    Main.js에서 누락된 community 링크 추가
    lamp 아이콘, logo 아이콘 변경

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import Account from "./pages/Account";
 import MyDrawer from "./pages/MyDrawer";
 import Test from "./pages/Test";
 import Bestseller from "./pages/Bestseller";
-//import Community from "./pages/Community";
+import Community from "./pages/Community";
 
 function App() {
   return (<div className="App">
@@ -18,17 +18,8 @@ function App() {
       <Route path="/myDrawer" element={<MyDrawer />} />
       <Route path="/test" element={<Test />} />
       <Route path="/bestseller" element={<Bestseller />} />
-      {/* <Route path="/community" element={<Community />} /> */}
+      <Route path="/community" element={<Community />} />
     </Routes>
-    <div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/login"}>Login</Link>
-      <Link to={"/account"}>Account</Link>
-      <Link to={"/myDrawer"}>mydrawer</Link>
-      <Link to={"/test"}>test</Link>
-      <Link to={"/bestseller"}>bestseller</Link>
-      {/* <Link to={"/community"}>community</Link> */}
-    </div>
     </div>
   );
 }
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Main.css";
 import logo from "../assets/logo.png";
+import lamp from "../assets/lamp.png";
 
 const Main = () => {
   return (
     <div className="main-container">
       <header className="header">
+        <div className="img-group">
+        <img src={lamp} className="lamp"/>
+        <img src={lamp} className="lamp" />
+        <img src={lamp} className="lamp" />
+        </div>
         <div className="nav-group">
           <div className="nav-item">
             <Link to="/account">회원가입</Link>
@@ -31,9 +37,9 @@ const Main = () => {
       </div>
 
       <div className="button-group">
-        <Link to="/bestseller" className="btn brown">이달의 베스트셀러</Link>
+        <Link to="/bestseller" className="btn brown">베스트셀러</Link>
         <Link to="/test" className="btn green">북루미 테스트</Link>
-        <Link to="/create" className="btn brown">북작북작</Link>
+        <Link to="/community" className="btn brown">북작북작</Link>
       </div>
     </div>
   );
