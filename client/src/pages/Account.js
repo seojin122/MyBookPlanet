f69ae877015e8ec2119ebe34f37770ec00072866
차차 <0409democracy@gmail.com>
2025-02-13 00:29:52 +0900

    feat : Add account&login page UI
    회원가입, 로그인 페이지 UI 구현
    Login.js에는 임시 이벤트핸들러 추가

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,4 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Account.css";
+import bookIcon from "../assets/bookicon.png";
+import lamp from "../assets/lamp.png";
+
 const Account = () => {
-    return <div>페이지입니다</div>
-}
+  return (
+    <div className="main-container">
+      <header className="header">
+        <div className="img-group">
+        <img src={lamp} className="lamp"/>
+        <img src={lamp} className="lamp" />
+        <img src={lamp} className="lamp" />
+        </div>
+        <div className="nav-group">
+          <div className="nav-item">
+            <Link to="/account">회원가입</Link>
+            <img src={bookIcon} className="book-icon"/>
+          </div>
+          <div className="nav-item">
+            <Link to="/login">로그인</Link>
+            <div className="underline"></div>
+          </div>
+          <div className="nav-item">
+            <Link to="/myDrawer">나의 서랍</Link>
+            <div className="underline"></div>
+          </div>
+        </div>
+      </header>
+
+      <div className="login-container">
+        <div className="login-box">
+          <div className="login-header">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/847/847969.png"
+
+              className="login-icon"
+            />
+            <h2>회원가입</h2>
+          </div>
+
+          <div className="login-form">
+            <label>이메일</label>
+            <input type="email" className="login-input" />
+
+            <label>닉네임</label>
+            <input type="text" className="login-input" />
+
+            <label>비밀번호</label>
+            <input type="password" className="login-input" />
+
+            <button className="login-button">회원가입</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Account;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,4 +1,89 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Login.css";
+import bookIcon from "../assets/bookicon.png";
+import lamp from "../assets/lamp.png";
+
 const Login = () => {
-    return <div>Login입니다</div>
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // 임시 폼제출 핸들러
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("로그인 시도:", { email, password });
+  };
+
+  return (
+    <div className="main-container">
+      <header className="header">
+        <div className="img-group">
+          <img src={lamp} className="lamp" alt="lamp" />
+          <img src={lamp} className="lamp" alt="lamp" />
+          <img src={lamp} className="lamp" alt="lamp" />
+        </div>
+        <div className="nav-group">
+          <div className="nav-item">
+            <Link to="/account">회원가입</Link>
+            <div className="underline"></div>
+          </div>
+          <div className="nav-item">
+            <Link to="/login">로그인</Link>
+            <img src={bookIcon} className="book-icon" alt="book icon" />
+          </div>
+          <div className="nav-item">
+            <Link to="/myDrawer">나의 서랍</Link>
+            <div className="underline"></div>
+          </div>
+        </div>
+      </header>
+
+      <div className="login-container">
+        <div className="login-box">
+          <div className="login-header">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/847/847969.png"
+              className="login-icon"
+              alt="user icon"
+            />
+            <h2>로그인</h2>
+          </div>
+
+          <form className="login-form" onSubmit={handleSubmit}>
+            <label>이메일</label>
+            <input
+              type="email"
+              className="login-input"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+
+            <label>비밀번호</label>
+            <input
+              type="password"
+              className="login-input"
+              value={password}
+              onChange={handlePasswordChange}
+              required
+            />
+
+            <button type="submit" className="login-button">
+              로그인
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
