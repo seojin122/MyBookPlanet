8982afff974d86c09d317bc4143ff20ff426a91e
Marin <akflsdlek@naver.com>
2025-02-14 01:58:33 +0900

    Initial commit on feature/seojin

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import MyDrawer from "./pages/MyDrawer";
+import Test from "./pages/Test";
+import Bestseller from "./pages/Bestseller";
+import Community from "./pages/Community";
+
+function App() {
+  return (
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/myDrawer" element={<MyDrawer />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="/bestseller" element={<Bestseller />} />
+      <Route path="/community" element={<Community />} />
+    </Routes>
+    </div>
+  );
+}
+export default App;
