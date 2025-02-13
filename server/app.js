920d605646971c88d738d86470d55b667657c602
inyeongjang <726jang@gmail.com>
2025-02-14 07:05:34 +0900

    etc

--- server/app.js
+++ server/app.js
@@ -4,19 +4,20 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const dotenv = require('dotenv');
 const path = require('path');
-
 dotenv.config();
 const indexRouter = require('./routes');
 const userRouter = require('./routes/user');
-// 설문 라우터 추가 
-const booklumiRouter = require('./routes/booklumi');
-// 책 추천 라우터 추가  
-const bookRecommendationRouter = require('./routes/bookRecommendation');  
+
+
+/* routes */
+const booklist = require('./routes/booklist');
+// const bookrecommend = require('./routes/bookrecommend');
+const booklumi = require('./routes/booklumi'); 
+
 
 const app = express();
 app.use('/api', indexRouter);  //FE에서 작성
 app.set('port', process.env.PORT || 3002);  //node 서버가 사용할 포트 번호, 리액트의 포트번호(3000)와 충돌하지 않게 다른 번호로 할당
-
 app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -35,10 +36,13 @@ app.use(session({
 
 app.use('/', indexRouter);
 app.use('/user', userRouter);
-// booklumi 라우터 등록 
-app.use('/api/booklumi', booklumiRouter); 
-// 책 추천 라우터 등록 
-app.use('/api/bookRecommendation', bookRecommendationRouter); 
+
+
+/* routes */
+app.use('/api/booklist', booklist);  // 도서 목록 API
+// app.use('/api/bookrecommend', bookrecommend);  // 도서 추천 API
+app.use('/api/booklumi', booklumi);  // booklumi API (독자 유형 저장)
+
 
 app.use((req, res, next) => {
   res.status(404).send('Not Found');
--- server/models/BooklumiTest.js
+++ server/models/BooklumiTest.js
@@ -1,16 +1,19 @@
-/* models/BooklumiTest.js */
-
 /* 독자 유형을 저장하는 모델 */ 
 
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');  // 데이터베이스 연결을 가져옵니다.
 
-const BooklumiTest = sequelize.define("BooklumiTest", {
-    userId: { type: DataTypes.INTEGER, allowNull: true }, 
-    
-    bookType: { type: DataTypes.STRING, allowNull: true },  // 도서 카테고리명 
-    categoryID: {type:DataTypes.INTEGER, allowNull: true},  // 도서 카테고리 ID 
-    readerType: { type: DataTypes.STRING, allowNull: true } // 독자 유형});
+// booklumitest 모델 정의 (독자 유형을 저장)
+const BooklumiTest = sequelize.define('BooklumiTest', {
+    userId: { 
+        type: DataTypes.INTEGER,  // 사용자 ID (null 가능)
+        allowNull: true
+    },
+    readerType: { 
+        type: DataTypes.STRING,  // 독자 유형 (null 가능)
+        allowNull: true
+    }
 });
 
+// 모델을 외부에서 사용할 수 있도록 export
 module.exports = BooklumiTest;
