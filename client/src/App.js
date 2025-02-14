420f62d4eac40033742b985553b8b6152b053436
차차 <0409democracy@gmail.com>
2025-02-15 00:01:12 +0900

    feat:fe 브랜치 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-//App.js
-import './App.css';
-import axios from "axios";
-import {useEffect} from "react";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+
+import Main from "./pages/Main";
+import Login from "./pages/Login";
+import Account from "./pages/Account";
+import MyDrawer from "./pages/MyDrawer";
+import Test from "./pages/Test";
+import Bestseller from "./pages/Bestseller";
+import Community from "./pages/Community";
+import BookDetail from "./pages/BookDetail";
 
 function App() {
-  const callApi = async () => {
-    axios.get('/api')
-    .then((res) => {
-      console.log(res.data.test)
-    })
-    .catch((err) => {
-      console.log(err);
-    })
-  };
-
-  useEffect(() => {
-    callApi();
-  }, []);
-
   return (
-   <div>test react</div>
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/myDrawer" element={<MyDrawer />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="/bestseller" element={<Bestseller />} />
+      <Route path="/book/:title" element={<BookDetail />} />
+      <Route path="/community" element={<Community />} />
+    </Routes>
+    </div>
   );
 }
-
 export default App;
